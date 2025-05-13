250771cc2befb9e67bfff900f678ea2bf963713c
AndreRicardoé <contatoadmx@gmail.com>
2025-05-13 13:35:56 -0300

    migration unsigned fx

--- app/Database/Migrations/2025-05-13-142410_UserTable.php
+++ app/Database/Migrations/2025-05-13-142410_UserTable.php
@@ -4,16 +4,16 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class User extends Migration
+class UserTable extends Migration
 {
     public function up()
     {
         $this->forge->addField([
-            'id'          => ['type' => 'INT', 'auto_increment' => true],
-            'email'       => ['type' => 'VARCHAR', 'constraint' => 255],
+            'id' => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
+            'email' => ['type' => 'VARCHAR', 'constraint' => 255],
             'password_hash' => ['type' => 'VARCHAR', 'constraint' => 255],
-            'created_at'  => ['type' => 'DATETIME', 'null' => true],
-            'updated_at'  => ['type' => 'DATETIME', 'null' => true],
+            'created_at' => ['type' => 'DATETIME', 'null' => true],
+            'updated_at' => ['type' => 'DATETIME', 'null' => true],
         ]);
         $this->forge->addKey('id', true);
         $this->forge->createTable('user');
