a2dd1ac8ddc064690842365e263cc7f229140806
AndreRicardoé <contatoadmx@gmail.com>
2025-05-13 11:45:18 -0300

    migration user

---
+++ app/Database/Migrations/2025-05-13-142410_User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => ['type' => 'INT', 'auto_increment' => true],
+            'email'       => ['type' => 'VARCHAR', 'constraint' => 255],
+            'password_hash' => ['type' => 'VARCHAR', 'constraint' => 255],
+            'created_at'  => ['type' => 'DATETIME', 'null' => true],
+            'updated_at'  => ['type' => 'DATETIME', 'null' => true],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('user');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user');
+    }
+}
